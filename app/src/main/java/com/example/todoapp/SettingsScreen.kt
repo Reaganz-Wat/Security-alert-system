a765fb142a0cb25963b95eed729e43e4869a44c7
Reaganz-Wat <reaganwatmon6@gmail.com>
2025-02-18 12:16:25 -0800

    tested broadcase, services

--- app/src/main/java/com/example/todoapp/SettingsScreen.kt
+++ app/src/main/java/com/example/todoapp/SettingsScreen.kt
@@ -1,6 +1,9 @@
 package com.example.todoapp
 
+import android.content.Intent
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -27,9 +30,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
+import androidx.wear.compose.material.Button
+import androidx.wear.compose.material.OutlinedButton
 
 @OptIn(ExperimentalMaterial3Api::class)
 //@Preview(showSystemUi = true, showBackground = true)
@@ -60,6 +67,8 @@ fun SettingsScreen(navigation: NavHostController) {
 @Composable
 fun SettingsContent(modifier: Modifier) {
 
+    val context = LocalContext.current
+
     Column (modifier = modifier) {
         Spacer(modifier = Modifier.height(10.dp))
         TextWithMaterialTheme("Emergency Contacts")
@@ -69,6 +78,21 @@ fun SettingsContent(modifier: Modifier) {
         TextWithMaterialTheme("Audio Capture")
         CardWithCheckBox("Enable Audio Recording")
         CardWithCheckBox("Recording Duration")
+
+        Row (horizontalArrangement = Arrangement.SpaceAround) {
+            OutlinedButton(onClick = {
+                Toast.makeText(context, "This is a toast", Toast.LENGTH_SHORT).show()
+                val intent = Intent(context, NotificationService::class.java)
+                ContextCompat.startForegroundService(context, intent)
+            }) {
+                Text("Start Service")
+            }
+            OutlinedButton(onClick = {
+//                context.stopService()
+            }) {
+                Text("Stop Service")
+            }
+        }
 
     }
 }
