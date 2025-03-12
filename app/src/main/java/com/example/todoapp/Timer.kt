9fc256330138be583a2602e41e0928d7a6a6bdb4
Reaganz-Wat <reaganwatmon6@gmail.com>
2025-03-13 00:05:15 +0300

    feat: implement timers features

--- app/src/main/java/com/example/todoapp/Timer.kt
+++ app/src/main/java/com/example/todoapp/Timer.kt
@@ -1,11 +1,115 @@
 package com.example.todoapp
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import androidx.wear.compose.material.Text
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Timer(navHostController: NavHostController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Timer")
+                },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        navHostController.popBackStack()
+                    }) {
+                        Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Arrow Back")
+                    }
+                }
+            )
+        }
+    ) {
+            innerPadding -> TimerMainContent(modifier = Modifier.padding(innerPadding))
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ShowTimerContent() {
+    TimerContent()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TimerContent() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Timer", color = Color.Black)
+                },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Arrow Back")
+                    }
+                }
+            )
+        }
+    ) {
+        innerPadding -> TimerMainContent(modifier = Modifier.padding(innerPadding))
+    }
+}
 
 @Composable
-fun Timer(navHostController: NavHostController, modifier: Modifier = Modifier) {
-    Text("This is a cool composable")
+fun TimerMainContent(modifier: Modifier = Modifier) {
+    var timer by remember { mutableStateOf("") }
+    Box(
+        modifier = modifier.fillMaxSize().padding(top = 10.dp, start = 10.dp),  // Fill the whole screen
+    ){
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+
+            TextField(
+                value = timer,
+                onValueChange = { timer = it },
+                placeholder = { Text("Enter Number") }
+            )
+            Button(onClick = {}) {
+                Text("Save")
+            }
+        }
+    }
 }
+
