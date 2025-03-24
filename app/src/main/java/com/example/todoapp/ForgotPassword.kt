f06ef7df12756ccb6e1fc70e8030a842ef10db95
Reaganz-Wat <reaganwatmon6@gmail.com>
2025-03-24 11:03:04 +0300

    feat: add forgotpassword

--- app/src/main/java/com/example/todoapp/ForgotPassword.kt
+++ app/src/main/java/com/example/todoapp/ForgotPassword.kt
@@ -1,9 +1,59 @@
 package com.example.todoapp
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 
 @Composable
-fun ForgotPassword(navHostController: NavHostController) {
+fun ForgotPassword(navHostController: NavHostController?) {
+    var email by remember { mutableStateOf("") }
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Forgot Password", fontSize = 24.sp, color = MaterialTheme.colorScheme.primary)
+        Spacer(modifier = Modifier.height(20.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            modifier = Modifier.fillMaxWidth(),
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Email, contentDescription = "UserName")
+            }
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick = { /* Handle password reset */ },
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(10.dp)
+        ) {
+            Text("Reset Password")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ForgotPasswordPreview() {
+    ForgotPassword(null)
 }
