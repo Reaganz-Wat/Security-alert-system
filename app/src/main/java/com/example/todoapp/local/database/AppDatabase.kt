4d23a1d0e3730b5ac487fa7be188a2ef0b85d25f
Reaganz-Wat <reaganwatmon6@gmail.com>
2025-03-26 07:09:27 +0300

    feat: add updated db

---
+++ app/src/main/java/com/example/todoapp/local/database/AppDatabase.kt
@@ -0,0 +1,60 @@
+package com.example.todoapp.local.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.todoapp.local.dao.AppSettingsDao
+import com.example.todoapp.local.dao.ContactDao
+import com.example.todoapp.local.dao.MessageMediaDao
+import com.example.todoapp.local.dao.MessageRecipientDao
+import com.example.todoapp.local.dao.SOSMessageDao
+import com.example.todoapp.local.dao.TimerDao
+import com.example.todoapp.local.dao.UserDao
+import com.example.todoapp.local.entities.AppSettingsEntity
+import com.example.todoapp.local.entities.ContactEntity
+import com.example.todoapp.local.entities.MessageMediaEntity
+import com.example.todoapp.local.entities.MessageRecipientEntity
+import com.example.todoapp.local.entities.SOSMessageEntity
+import com.example.todoapp.local.entities.TimerEntity
+import com.example.todoapp.local.entities.UserEntity
+
+// Main Database Class
+@Database(
+    entities = [
+        AppSettingsEntity::class,
+        UserEntity::class,
+        ContactEntity::class,
+        SOSMessageEntity::class,
+        MessageMediaEntity::class,
+        MessageRecipientEntity::class,
+        TimerEntity::class
+    ],
+    version = 1
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun appSettingsDao(): AppSettingsDao
+    abstract fun userDao(): UserDao
+    abstract fun contactDao(): ContactDao
+    abstract fun sosMessageDao(): SOSMessageDao
+    abstract fun messageMediaDao(): MessageMediaDao
+    abstract fun messageRecipientDao(): MessageRecipientDao
+    abstract fun timerDao(): TimerDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
